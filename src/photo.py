7867a3dcf47e03d9f30e5387aae8311a253e06fd
Maxim <bill03051990@gmail.com>
2025-10-24 11:56:08 +0300

    Add Function visualize. Add graphs

--- src/photo.py
+++ src/photo.py
@@ -1,8 +1,34 @@
 from PIL import Image
 import random
 import os
-import torch
 import torchvision.transforms as t
+from torchvision.transforms import ToPILImage
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+def visualize(image_path, tensor_image):
+    image = Image.open(image_path)
+    gray_image = image.convert('L')
+    to_pil = ToPILImage()
+    final_image = to_pil(tensor_image)
+    fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+
+    axes[0].imshow(image)
+    axes[0].set_title("Оригинал")
+    axes[0].axis("off")
+
+    axes[1].imshow(gray_image, cmap="gray")
+    axes[1].set_title("Grayscale")
+    axes[1].axis("off")
+
+    axes[2].imshow(final_image)
+    axes[2].set_title("После преобразований")
+    axes[2].axis("off")
+
+    plt.tight_layout()
+    plt.savefig("visualize.jpg", dpi=1600, bbox_inches='tight')
+
 
 # Photo
 rand_folder = random.randint(1, 7)
@@ -16,9 +42,8 @@ image_path = os.path.join(folder, random_image)
 
 photo = Image.open(image_path)
 photo_data = list(photo.getdata())
-print(photo.size)
-print(photo.mode)
-# print(photo_data)
+# print(photo.size)
+# print(photo.mode)
 
 # photo
 
@@ -31,4 +56,26 @@ transform = t.Compose([
 ])
 
 transform_image = transform(photo)
-print(transform_image)
+# print(transform_image)
+
+data = pd.read_csv('data/train_labels.csv')
+
+result_wonder = (data['label'] == 1).sum()
+result_scared = (data['label'] == 2).sum()
+result_disgust = (data['label'] == 3).sum()
+result_happy = (data['label'] == 4).sum()
+result_sad = (data['label'] == 5).sum()
+result_angry = (data['label'] == 6).sum()
+result_neutral = (data['label'] == 7).sum()
+
+x = ['wonder', 'scared', 'disgust', 'happy', 'sad', 'angry', 'neutral']
+y = [result_wonder, result_scared, result_disgust, result_happy, result_sad, result_angry, result_neutral]
+
+plt.bar(x, y, label = 'Count')
+plt.xlabel('Emotions')
+plt.ylabel('quantity')
+plt.title('Data')
+plt.legend()
+# plt.savefig("plot.jpg", dpi=1600, bbox_inches='tight')
+
+visualize(image_path, transform_image)
