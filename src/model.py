5bd873364048e72b18a23acbcc9ac750d97b13e2
Maxim <bill03051990@gmail.com>
2025-11-21 14:53:49 +0300

    Add notebook about FC model. CNN model on going

--- src/model.py
+++ src/model.py
@@ -28,6 +28,30 @@ class SimpleFcModel(nn.Module):
         x = self.fc2(x)
         x = self.relu(x)
         return x
+    
+class SimpleCNNModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.feauter_extractor = nn.Sequential(
+            nn.Conv2d(1, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(16, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.classfier = nn.Sequential(
+            nn.Flatten(),
+            #nn.Linear(, 7)
+        )
+
+    def forward(self, x):
+        x = self.feauter_extractor(x)
+        x = self.classfier(x)
+        return x
 
 
 #инициализируем размер изображения и саму модель
@@ -53,20 +77,34 @@ history = {
 for epoch in range(n_epoch):
     print(f"\nEPOCH {epoch+1}/{n_epoch}")
     train_loss, train_acc, train_f1 = epo.train_epoch(model, dt.train_loader, optimizer, criterion, device)
+    print(f"Train loss: {train_loss}")
+    print(f"Train accuracy: {train_acc}")
+    print(f"Train f1: {train_f1}")
     test_loss, test_acc, test_f1 = epo.evaluate_epoch(model, dt.test_loader, criterion, device)
+    print(f"Test loss: {test_loss}")
+    print(f"Test accyracy: {test_acc}")
+    print(f"Test f1: {test_f1}")
 
-history["train_loss"].append(train_loss)
-history["test_loss"].append(test_loss)
-history["train_f1"].append(train_f1)
-history["test_f1"].append(test_f1)
-history["train_acc"].append(train_acc)
-history["test_acc"].append(test_acc)
+    history["train_loss"].append(train_loss)
+    history["test_loss"].append(test_loss)
+    history["train_f1"].append(train_f1)
+    history["test_f1"].append(test_f1)
+    history["train_acc"].append(train_acc)
+    history["test_acc"].append(test_acc)
+
+    if test_f1 > best_f1:
+        best_f1 = test_f1
+        best_epoch = epoch
+        torch.save(model.state_dict(), "models/best_model_fc.pth")
+        print(f"Epoch: {epoch+1}, F1: {best_f1:.3f}")
+
+print(f"Best model: Epoch {best_epoch+1}, F1: {best_f1:.3f}")
 
 #вывод оценки test_f1 и train_f1
-print(f"Train F1: {train_f1:.3f} | Test F1: {test_f1:.3f}")
-if test_f1 > best_f1:
-    best_f1 = test_f1
-    torch.save(model.state_dict(), "models/best_model_fc.pth")
+# print(f"Train F1: {train_f1:.3f} | Test F1: {test_f1:.3f}")
+# if test_f1 > best_f1:
+#     best_f1 = test_f1
+#     torch.save(model.state_dict(), "models/best_model_fc.pth")
 
 #сохранение графика
 plt.plot_history(history)
--- src/plots.py
+++ src/plots.py
@@ -13,18 +13,18 @@ def plot_history(history):
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
     
-    ax1.plot(epochs, history['train_loss'], label='Train Loss', marker='o')
-    ax1.plot(epochs, history['test_loss'], label='Test Loss', marker='o')
+    ax1.plot(epochs, history['train_loss'], label='Train Loss')
+    ax1.plot(epochs, history['test_loss'], label='Test Loss')
     ax1.legend(); ax1.set_title('Loss')
     ax1.set_xlabel('Epochs'); ax1.set_ylabel('Loss')
     
-    ax2.plot(epochs, history['train_acc'], label='Train Acc', marker='o')
-    ax2.plot(epochs, history['test_acc'], label='Test Acc', marker='o')
+    ax2.plot(epochs, history['train_acc'], label='Train Acc')
+    ax2.plot(epochs, history['test_acc'], label='Test Acc')
     ax2.legend(); ax2.set_title('Accuracy')
     ax2.set_xlabel('Epochs'); ax2.set_ylabel('Accuracy')
     
-    ax3.plot(epochs, history['train_f1'], label='Train F1', marker='o')
-    ax3.plot(epochs, history['test_f1'], label='Test F1', marker='o')
+    ax3.plot(epochs, history['train_f1'], label='Train F1')
+    ax3.plot(epochs, history['test_f1'], label='Test F1')
     ax3.legend(); ax3.set_title('F1 Score')
     ax3.set_xlabel('Epochs'); ax3.set_ylabel('F1 Score')
     
